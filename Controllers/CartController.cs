c7ef2da2aa448ae2becbc6daf132e98f4c120601
musa jahun <msjahun@live.com>
2019-12-27 23:10:15 +0200

    added all files

---
+++ Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HerbsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HerbsStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+  
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
