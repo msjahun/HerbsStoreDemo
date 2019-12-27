ccbdfd40c6544185e6a381ab94166a9655189f2d
musa jahun <msjahun@live.com>
2019-12-27 23:01:55 +0200

    Add project files.

---
+++ HerbsStore/Controllers/AdministrationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HerbsStore.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult ProductAdd()
+        {
+            return View();
+        }
+
+        public IActionResult ProductEdit()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public IActionResult OrderEdit()
+        {
+            return View();
+        }
+
+        public IActionResult OrderConfirm()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Hospitals()
+        {
+            return View();
+        }
+    }
+}
